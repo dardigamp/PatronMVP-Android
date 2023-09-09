1a765bad7472471e633125c97275e38a510e55cb
Darwin Díaz <dardigamp@gmail.com>
2023-09-08 19:05:49 -0500

    Primer commit

---
+++ app/src/main/java/com/github/calo001/interactor/OperationInteractorImpl.java
@@ -0,0 +1,41 @@
+package com.github.calo001.interactor;
+
+import com.github.calo001.interfaces.OperationInteractor;
+import com.github.calo001.interfaces.OperationPresenter;
+
+public class OperationInteractorImpl implements OperationInteractor {
+    private int result;
+    private OperationPresenter presenter;
+
+    public OperationInteractorImpl(OperationPresenter operationPresenter) {
+        this.presenter = operationPresenter;
+    }
+
+    @Override
+    public void add(int num1, int num2) {
+        result =  num1 + num2;
+        presenter.showResult(Integer.toString(result));
+    }
+
+    @Override
+    public void subtract(int num1, int num2) {
+        result = num1 - num2;
+        presenter.showResult(Integer.toString(result));
+    }
+
+    @Override
+    public void multiply(int num1, int num2) {
+        result = num1 * num2;
+        presenter.showResult(Integer.toString(result));
+    }
+
+    @Override
+    public void divide(int num1, int num2) {
+        if (num2 == 0) {
+            presenter.invalidOperation();
+        } else {
+            result = num1 / num2;
+            presenter.showResult(Integer.toString(result));
+        }
+    }
+}
